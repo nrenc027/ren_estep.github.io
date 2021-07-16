25ad5350be2f5d0e28cee976ec7152fb3e9a5447
Ren Estep <n.ren.estep@gmail.com>
2021-07-16 17:17:06 -0400

    hash router

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AboutPage from "./about";
 import Home from "./home";
 import ContactPage from "./contact";

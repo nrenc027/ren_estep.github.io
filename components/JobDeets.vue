f5f1822cc70ed667ad3f22b4a1045b354108dd77
Ren Estep <n.ren.estep@gmail.com>
2022-02-11 22:32:07 -0500

    resume updates

---
+++ components/JobDeets.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+    <article v-for="experience in experiences" :key="experience">
+      <h3>{{ experience.business }}</h3>
+      <dl v-for="position in experience.positions" :key="position">
+        <dt>{{ position.title }}</dt>
+        <dd>
+          <li v-for="deet in position.deets" :key="deet">{{ deet }}</li>
+        </dd>
+      </dl>
+    </article>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    experiences: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
